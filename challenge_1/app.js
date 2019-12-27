dec023c1ab284e02d63f78b241934968858759f4
henimezrani <henimezrani@gmail.com>
2019-12-27 11:15:42 +0100

    Game init board working

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -19,6 +19,18 @@ class Game {
 
   init() {
 
+    // Initialize Empty 3x3 Board
+    this.board = new Array(3);
+    for (var i = 0; i < this.board.length; i++) {
+      this.board[i] = new Array(3);
+    }
+
+    // Fill empty board with 0
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+          this.board[i][j] = 0;
+      }
+    }
   }
 
   check() {
