06c60801fa5b291e4f3c445a87a99244c66b6341
henimezrani <henimezrani@gmail.com>
2019-12-27 11:10:53 +0100

    Game and Player classes and constructors created

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,43 @@
+class Player {
+  constructor(name, score, symbol) {
+    this.name = name;
+    this.score = score;
+    this.symbol = symbol
+  }
 
+  playMove() {
+
+  }
+}
+
+class Game {
+  constructor() {
+    this.board = []
+    this.players = []
+    this.gamesCount = []
+  }
+
+  init() {
+
+  }
+
+  check() {
+
+  }
+
+  rowWin() {
+
+  }
+
+  colWin() {
+
+  }
+
+  diagWin(){
+
+  }
+
+  reset() {
+
+  }
+}
