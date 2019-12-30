671d00869f5ba8bfd96b16fb3dcb64005f69b5e7
henimezrani <henimezrani@gmail.com>
2019-12-30 09:51:19 +0100

    challenge 2 bare minimum, needs improvement

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,40 @@
+$('#submit').click(function (e) {
+  console.log('here')
+  e.preventDefault();
 
+  var JSONtext = $("#JSONtext").val();
+  var JSONfileElem = document.getElementById('JSONfile');
 
+  if(JSONtext !== '') { // there is text, handle it
+    $.post("http://localhost:8080/data", {
+      JSONdata: JSONtext
+    })
+  } else if (JSONfileElem.files.length !== 0) { // there is a file, handle it
+    
+    var reader = new FileReader();
+    reader.onload = (e) => {
+      var JSONfile = e.target.result;
+      $.post("http://localhost:8080/data", {
+        JSONdata: JSONfile
+      }).done((data) => {
+        $('body').append(renderResult(data));
+      })
+    };
+    reader.readAsText(JSONfileElem.files[0]);
+  } else {
+    alert('please enter a text or choose a file')
+  }
+});
+
+function renderResult(data) {
+
+  var statsDiv = document.createElement("DIV");
+  var nameDiv = document.createElement("H2");
+  var dataDiv = document.createElement("P");
+  nameDiv.innerHTML = "Converted File"
+  dataDiv.innerHTML = data
+  statsDiv.appendChild(nameDiv);
+  statsDiv.appendChild(dataDiv);
+
+  return statsDiv
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,60 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var $ = require("jquery");
 
+var PORT = process.env.PORT || 8080
+var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/client'));
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.post("/data", (req, res) => {
+	var data = JSON.parse(req.body.JSONdata)
+	var convertedData = convertJSONtoCSV(data);
+	res.send(convertedData)
+});
+
+app.listen(PORT, () => {
+  console.log(`running on: http://localhost:${PORT}`);
+});
+
+function convertJSONtoCSV(JSONObject){
+	var ArrayData = retrieveData(JSONObject);
+	for (var i = 0 ; i < ArrayData.length ; i++) {
+		ArrayData[i] = ArrayData[i].join(',')
+	}
+	return ArrayData.join('\n')
+
+	function retrieveData(JSONObject, result, firstTime) {
+		var firstTime = firstTime || 0; 
+		var result = result || []
+		var tmpArr = [];
+		if (firstTime === 0) {
+			var keysArr = [];
+			for (var key in JSONObject) {
+				if (key !== 'children') {
+					keysArr.push(key)
+				}
+			}
+			result.push(keysArr)
+		}
+
+		for (var key in JSONObject) {
+			if (key !== 'children'){
+				tmpArr.push(JSONObject[key])
+			} else {
+				result.push(tmpArr)
+				for (var i = 0 ; i < JSONObject.children.length ; i++) {
+					retrieveData(JSONObject.children[i], result, 1)
+				}
+				
+			}
+		}
+		return result;
+	}
+}
